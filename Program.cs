9b95b519bbdb62f70e2f075aca908e37252540c7
danil54543 <danil54543@gmail.com>
2025-05-11 15:55:37 +0300

    add HCaptchaService add Captcha page add package Newtonsoft

--- Program.cs
+++ Program.cs
@@ -1,29 +1,39 @@
 using Microsoft.EntityFrameworkCore;
 using Server.Data;
+using Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddMvc();
 
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IHCaptchaService, HCaptchaService>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage(); // сюда
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
+app.UseRouting();
 
 app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Captcha}/{action=Index}/{id?}");
 
 app.Run();
 
