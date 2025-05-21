abd163db008547c654d06561f63db6eb3ccf7d67
danil54543 <danil54543@gmail.com>
2025-05-21 19:43:10 +0300

    add StackExchange.Redis

--- Hubs/DeviceProvisioningHub.cs
+++ Hubs/DeviceProvisioningHub.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System.Text.RegularExpressions;
 
 namespace Server.Hubs
 {
     public class DeviceProvisioningHub : Hub
     {
-        // Группы = каналы
         public async Task SubscribeToProvisioningChannel(string channelId)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, channelId);
